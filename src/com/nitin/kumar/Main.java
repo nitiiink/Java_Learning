c1a0e79406a1de94c79fea69241a35d39cdd219e
nitink <nitin.kumar@ellucian.com>
2023-03-19 15:56:23 +0530

    added code for java basics

--- src/com/nitin/kumar/Main.java
+++ src/com/nitin/kumar/Main.java
@@ -1,7 +1,33 @@
 package com.nitin.kumar;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //primitve type
+        int age = 23;
+
+        //reference type
+        Date now = new Date();
+        System.out.println("The current date is" + now);
+
+        //eg for string
+        String test = "Hello" + "!!";
+        System.out.println(test.endsWith("!!"));
+
+        //escape sequence \ (backslash) (\n (nextLine) (\t tab)
+        System.out.println("C:\\Windows\\ProgramFiles");
+        System.out.print("this will print the line in next line \n");
+        System.out.print("this will tab \t everything next to it \n");
+
+        //Arrays
+        int[] numbers = new int[5];
+        numbers[0] = 10;
+        numbers[1] = 12;
+        System.out.println(Arrays.toString(numbers));
     }
 }
